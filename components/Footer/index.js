1a155b7c48656ee3954ff021e4b43d3ccac8e842
DONTKILLME <ashishnegi0511@gmail.com>
2024-03-04 10:19:37 +0530

    added footer

---
+++ components/Footer/index.js
@@ -0,0 +1,244 @@
+import {
+  ButtonGroup,
+  Container,
+  IconButton,
+  Stack,
+  Text,
+  Image,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  Button,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
+
+export const Footer = () => (
+  <Container
+    as="footer"
+    role="contentinfo"
+    py={{
+      base: "12",
+      md: "16",
+    }}
+    bg="rgb(60, 65, 66)"
+    minWidth="100%"
+    position="relative"
+
+    // style={{ marginTop: "auto" }}
+  >
+    <Stack
+      spacing={{
+        base: "4",
+        md: "5",
+      }}
+      direction="row"
+    >
+      {/* Column No 1 */}
+      <Stack
+        spacing={{
+          base: "4",
+          md: "5",
+        }}
+        direction="column"
+      >
+        <Stack justify="space-between" direction="row" align="center">
+          {/* <Logo /> */}
+          <Image
+            src={"/logo_orange.svg"}
+            alt="Logo"
+            height={300}
+            width={300}
+            style={{
+              marginLeft: "200px",
+              marginBlockStart: "-80px",
+            }}
+          />
+        </Stack>
+      </Stack>
+      {/* Column No 2 */}
+      <Stack
+        spacing={{
+          base: "4",
+          md: "5",
+        }}
+        direction="column"
+        style={{ marginLeft: "25px" }}
+      >
+        <List padding={1}>
+          <label>
+            <b>Company</b>
+          </label>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Why Gift Box?</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Our Story</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Careers</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Press</ListItem>
+          </Link>
+          <Link href={"/"}>
+            <ListItem mt={1}>FAQs</ListItem>
+          </Link>
+        </List>
+      </Stack>
+      {/* Column No 3 */}
+      <Stack
+        spacing={{
+          base: "4",
+          md: "5",
+        }}
+        direction="column"
+        style={{ marginLeft: "25px" }}
+      >
+        <List padding={1}>
+          <label>
+            <b>Product</b>
+          </label>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>How it works?</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Pricing</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Use Cases</ListItem>
+          </Link>
+          <Link href={"/"}>
+            <ListItem mt={1}>Integrations</ListItem>
+          </Link>
+        </List>
+      </Stack>
+      {/* Column No 4 */}
+      <Stack
+        spacing={{
+          base: "4",
+          md: "5",
+        }}
+        direction="column"
+        style={{ marginLeft: "25px" }}
+      >
+        <List padding={1}>
+          <label>
+            <b>Resources</b>
+          </label>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Blog</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Partnerships</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Case studies</ListItem>
+          </Link>
+          <Link href={"/"}>
+            <ListItem mt={1}>Media Assets</ListItem>
+          </Link>
+        </List>
+      </Stack>
+      {/* Column No 5 */}
+      <Stack
+        spacing={{
+          base: "4",
+          md: "5",
+        }}
+        direction="column"
+        style={{ marginLeft: "25px" }}
+      >
+        <List padding={1}>
+          <label>
+            <b>Legal</b>
+          </label>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Terms of Service</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Privacy Policy</ListItem>
+          </Link>
+          <Link href={"/"}>
+            {" "}
+            <ListItem mt={1}>Offer terms</ListItem>
+          </Link>
+          <Link href={"/"}>
+            <ListItem mt={1}>License</ListItem>
+          </Link>
+        </List>
+      </Stack>
+      {/* Column No 6 */}
+      <Stack spacing={{ base: "4", md: "5" }}>
+        <label>
+          <b>Stay up to date</b>
+        </label>
+        <Stack justify="space-between" direction="row" align="center">
+          <Input placeholder="Enter your email" />
+
+          <Button size={"md"} colorScheme="teal" variant="outline">
+            Subscribe
+          </Button>
+        </Stack>
+      </Stack>
+    </Stack>
+
+    <Box as="hr" bg={"grey"} />
+
+    <Stack spacing={{ base: "4", md: "5" }}>
+      <Stack
+        style={{ marginLeft: "200px" }}
+        justify="space-between"
+        direction="row"
+        align="center"
+      >
+        {" "}
+        <ButtonGroup style={{ marginTop: "40px" }} variant="tertiary">
+          <IconButton
+            _hover={{ color: `var(--primary-color)` }}
+            as="a"
+            href="#"
+            aria-label="LinkedIn"
+            icon={<FaLinkedin size="lg" />}
+          />
+          <IconButton
+            _hover={{ color: `var(--primary-color)` }}
+            as="a"
+            href="#"
+            aria-label="GitHub"
+            icon={<FaGithub size="lg" />}
+          />
+          <IconButton
+            _hover={{ color: `var(--primary-color)` }}
+            as="a"
+            href="#"
+            aria-label="Twitter"
+            icon={<FaTwitter size="lg" />}
+          />
+        </ButtonGroup>
+        <Text
+          style={{ marginRight: "200px", marginTop: "40px" }}
+          fontSize="lg"
+          color="fg.subtle"
+        >
+          &copy; {new Date().getFullYear()} Gift Box, All rights reserved.
+        </Text>
+      </Stack>
+    </Stack>
+  </Container>
+);
