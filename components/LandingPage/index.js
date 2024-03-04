9a54c53a35a13b35ce065038e877c02ef43247aa
DONTKILLME <ashishnegi0511@gmail.com>
2024-03-04 10:21:47 +0530

    Landing Page Development in Progress.

--- components/LandingPage/index.js
+++ components/LandingPage/index.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 // import Card from './Card/Card';
 // import Edition from './Card/Edition';
-import { Dropdown } from "@nextui-org/react";
+import { Dropdown, red } from "@nextui-org/react";
+import { ExploreNow } from "./ExploreNow";
+import { ExploreNowSingle } from "./ExploreNowSingle";
+import { AddToCartCard } from "./AddToCartCard";
+import { Footer } from "../Footer";
 // import { fetchNFTS } from '../fe_call/routes';
 // import Footer from './Footer/index.js';
 // import CompanyInfo from './NFTDisplay/CompanyInfo';
@@ -40,50 +44,22 @@ export default function LandingPage({ children, props }) {
   //   }
 
   return (
-    <>
-      {
-        <div
-          className="w-full flex flex-col items-center "
-          style={{ color: "black", marginTop: "5.5%" }}
-        >
-          <div
-            className="w-full flex flex-col "
-            style={{ borderRadius: "10px", backgroundColor: "blue" }}
-          >
-            {/* <CompanyInfo />
-            <Footer /> */}
-            TEST MY BOY{" "}
-          </div>
-          <div
-            className="w-full flex flex-col "
-            style={{ borderRadius: "10px", backgroundColor: "blue" }}
-          >
-            {/* <CompanyInfo />
-            <Footer /> */}
-            TEST MY BOY{" "}
-          </div>
-          <div
-            className="w-full flex flex-col "
-            style={{ borderRadius: "10px", backgroundColor: "blue" }}
-          >
-            {/* <CompanyInfo />
-            <Footer /> */}
-            TEST MY BOY{" "}
-          </div>
-          <div
-            className="w-full flex flex-col "
-            style={{ borderRadius: "10px", backgroundColor: "blue" }}
-          >
-            {/* <CompanyInfo />
-            <Footer /> */}
-            TEST MY BOY{" "}
-          </div>
-
-          <div className="text-3xl alteredprivacy inprivacy  ">
-            <span style={{ float: "left" }}>Profile: </span>
-          </div>
-        </div>
-      }
-    </>
+    <div
+      className="w-full flex flex-col items-center"
+      style={{ color: "black", background: "darkgray" }}
+    >
+      {/* <ShoppingComponent />
+      <ShoppingImageComponent /> */}
+      <ExploreNowSingle />
+      <AddToCartCard />
+      <ExploreNow />
+      <AddToCartCard />
+      <ExploreNowSingle />
+      <AddToCartCard />
+      <ExploreNow />
+      <AddToCartCard />
+      <ExploreNowSingle />
+      <Footer />
+    </div>
   );
 }
